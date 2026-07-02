ba88bd0f7de351c539b897ea88a523c2fc09983b
Everton Carneiro <everton.iosdev@gmail.com>
2026-07-02 10:08:58 -0300

    Initial Commit

---
+++ XCleanup/XCleanupApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCleanupApp.swift
+//  XCleanup
+//
+//  Created by Everton Lopes Carneiro on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct XCleanupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
